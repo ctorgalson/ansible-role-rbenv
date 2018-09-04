dfd56039df595225b6e0fc33587f0117e82d6230
Christopher Torgalson <manager@bedlamhotel.com>
2018-09-04 17:16:52 +0200

    First commit of some working tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,5 +1,7 @@
 import os
 
+import pytest
+
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
@@ -12,3 +14,31 @@ def test_hosts_file(host):
     assert f.exists
     assert f.user == 'root'
     assert f.group == 'root'
+
+
+@pytest.mark.parametrize("name", [
+    "lorem",
+])
+def test_rbenv_install(host, name):
+    f = host.file("/home/{0}/.rbenv".format(name))
+
+    assert f.exists
+    assert f.is_directory
+    assert f.user == name
+    assert f.group == name
+
+
+@pytest.mark.parametrize("name", [
+    "lorem",
+])
+@pytest.mark.parametrize("plugin", [
+    "rbenv-default-gems",
+    "ruby-build",
+])
+def test_rbenv_plugin_install(host, name, plugin):
+    f = host.file("/home/{0}/.rbenv/plugins/{1}".format(name, plugin))
+
+    assert f.exists
+    assert f.is_directory
+    assert f.user == name
+    assert f.group == name
